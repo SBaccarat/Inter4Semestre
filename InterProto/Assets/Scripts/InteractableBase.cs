64c84e1de00cf506e61f2888d39bdf95b6e085a7
Unknown <yan_sao.paulo@hotmail.com>
2018-03-26 17:47:07 -0300

    Prototipo PointInClick
    Mecanicas: Movimentaçao, Interaçao PointInClic basica, Inventario Basico, transiçao interna de cenas

---
+++ InterProto/Assets/Scripts/InteractableBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableBase : MonoBehaviour {
+
+   protected bool interactTime; // indica quando o item esta interativo
+
+    // funçao que checa o clique no objeto
+    private void OnMouseDown()
+    {
+        StartCoroutine("CheckPlayer");
+    }
+
+    // funçao que mantem o item interativel por serto tempo
+    IEnumerator CheckPlayer()
+    {
+        interactTime = true;
+        yield return new WaitForSeconds(5);
+        interactTime = false;
+    }
+
+}
---
+++ InterProto/Assets/Scripts/Persistence.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Persistence {
+
+    // salva o status do item, pego ou n
+    public static bool itemAvailable = true;
+
+    //salva a posiçao em que o player vai aparecer
+    public static Vector2 NewPos = new Vector2(0, -3);
+
+}
