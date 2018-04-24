113ed9932146213f5c954bf30146a664390e7561
Matheus <matheus.mello@vetorzero.com.br>
2018-04-24 17:29:16 -0300

    come;ando escolha aleatoria da sequancia de cores

---
+++ InterProto/Assets/Scripts/MiniGame/ChoseSequence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoseSequence : MonoBehaviour
+{
+
+	public Image[] BolinhasPrefbs;
+
+	private Image[] _bolinhasOrder;
+	// Use this for initialization
+	void Start ()
+	{
+		_bolinhasOrder = new Image[5];
+		
+		EscolheSequencia();
+	}
+
+	private void EscolheSequencia()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
