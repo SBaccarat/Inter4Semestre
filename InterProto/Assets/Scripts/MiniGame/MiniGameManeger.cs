97e55926ecc4c6180e1ebcbe55e24f3750f06e7e
Unknown <junqueirademello@gmail.com>
2018-05-19 15:37:52 -0300

    melhorias minigame, incluindo musica e sons

--- InterProto/Assets/Scripts/MiniGame/MiniGameManeger.cs
+++ InterProto/Assets/Scripts/MiniGame/MiniGameManeger.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -10,7 +11,9 @@ public class MiniGameManeger : MonoBehaviour
 	public List<GameObject> Senha;
 	public GameObject Tutorial;
 	public static MiniGameManeger Instace;
-	private bool once= false;
+	public GameObject Florclicada;
+	public AudioSource _acertoAudio;
+	private bool _once= false;
 	
 	void Start ()
 	{
@@ -21,7 +24,6 @@ public class MiniGameManeger : MonoBehaviour
 		var list = FindObjectsOfType<ChoseColor>().ToList().OrderBy((a)=> a.name);
 		foreach (var choseColor in list)
 		{
-			Debug.Log(choseColor.name);
 			Senha.Add(choseColor.GetCores());
 		}
 		Invoke("SomeTutorial",4);
@@ -36,10 +38,9 @@ public class MiniGameManeger : MonoBehaviour
 	void Update ()
 	{
 		
-		if (Senha.Count<1&&once==false)
+		if (Senha.Count<1&&_once==false)
 		{
-			Debug.Log("sai");
-			once = true;
+			_once = true;
 
             //colocar aq para tocar a animação da baliarina dançando
             anim.SetBool("ativado", true);
@@ -59,8 +60,11 @@ public class MiniGameManeger : MonoBehaviour
 		var array = Senha.ToArray();
 		if (array[0].name.Equals(colorClicked))
 		{
+			_acertoAudio.Play();
+			Florclicada.transform.DOLocalRotate(Vector3.back*3600, 1f)
+				.OnComplete(() => Florclicada.transform.localPosition = Vector2.right * 100);
 			array[0].transform.parent.gameObject.SetActive(false);
 			Senha.RemoveAt(0);	
-		}	
+		}
 	}
 }
