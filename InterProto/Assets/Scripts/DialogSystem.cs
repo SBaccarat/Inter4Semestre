855ab9fb6e902f7273ef0e8d27094a824b325ea8
Unknown <fgalinskas@gmail.com>
2018-06-03 17:49:49 -0300

    update galinskas
    ok revisar TODOS os paineis (arte)
    ok trocar a fonte dos dialogos
    ok revisar os TEXTOS
    ok colocar o carrinho de bb na cena do quarto
    ok colocar o esquema no money bitch
    ok tirar o transition time no player
    ok idles muito freneticos, reduzir velocidade
    ok deixar trigger o collider do negao na cena exterior cortico

--- InterProto/Assets/Scripts/DialogSystem.cs
+++ InterProto/Assets/Scripts/DialogSystem.cs
@@ -53,23 +53,23 @@ public class DialogSystem : MonoBehaviour {
                     sentences[0] = "Porque demorou tanto em menino?";
                     sentences[1] = "Nem começa, tenho culpa não, os homi tavam tocando o povo da rua, não dava para passar! Achou que demorei? Vai você!";
                     sentences[2] = "E deixar você cuidando do Mauricio? Nem em sonho, seu moleque!";
-                    sentences[3] = "Eu não sou moleque não !!";
+                    sentences[3] = "Eu não sou moleque não!";
                     sentences[4] = "Ta ta ta, não enche as minhas ventas! Pega a sua irmã e leva pra tomar banho.";
                     sentences[5] = "Calma, eu tenho que fazer uma fita antes, é o tempo dela pegar a toalha e me encontrar na fila...";
-                    sentences[6] = "Oxi!! fazer o que moleque?? Cê sabe que tem que levar ela ";
-                    sentences[7] = "Não sou moleque!! E ela tambem não é mais criança. o caminho é facil, só decer a escada e ir pra esquerda até o patio!";
+                    sentences[6] = "Oxi!! fazer o que moleque?? Cê sabe que tem que levar ela";
+                    sentences[7] = "Não sou moleque! E ela tambem não é mais criança. O caminho é facil, só decer a escada e ir pra esquerda até o patio!";
                 }else
                 if (Persistence.SceneQuartoStatus == 3)
                 {
                     Character = Irmao;
                     OtherPerson = Irmã;
                     sentences = new string[7];
-                    sentences[0] = "Tem como voce me ajudar Everson?ou eu cozinho ou cuido do Mauricio";
-                    sentences[1] = "Ele não para de chorar eu já cansei";
+                    sentences[0] = "Tem como você me ajudar Everson? Ou eu cozinho ou cuido do Mauricio";
+                    sentences[1] = "Ele não para de chorar eu já cansei..";
                     sentences[2] = "Para de ser idiota menino, pega ele no colo!! AONDE VOCÊ PENSA QUE VAI???";
                     sentences[3] = "Eu vou sair pra fumar se vira ai...";
-                    sentences[4] = "Como assim? não, Everson!! EVERSON!! Você vai ver só quando a mãe voltar.";
-                    sentences[5] = "Nem to te ouvindo mais.. (voz de lonje)";
+                    sentences[4] = "Como assim? Não, Everson!! EVERSON!! Você vai ver só quando a mãe voltar..";
+                    sentences[5] = "Nem to te ouvindo mais.. (voz de longe)";
                     sentences[6] = "Stteffany me ajuda aqui menina, mexe a panela pra não queimar o almoço! Anda rápido!";
                     QuestLog.MainQuestStaus = 6;
                 }
@@ -79,7 +79,7 @@ public class DialogSystem : MonoBehaviour {
                     Character = Stte;
                     OtherPerson = Irmao;
                     sentences = new string[2];
-                    sentences[0] = "Vem Logo Stte!!! É a sua vez... Se nao andar logo vai ver só. ";
+                    sentences[0] = "Vem Logo Stte!!! É a sua vez... Se nao andar logo vai ver só..";
                     sentences[1] = "To indo...";      
             }else
             if (Persistence.Scene == 7)
@@ -88,8 +88,8 @@ public class DialogSystem : MonoBehaviour {
                 OtherPerson = Mae;
                 sentences = new string[3];
                 sentences[0] = "Meninas, Acordem!!";
-                sentences[1] = "Valesca: Precisa de alguma coisa mãe??/n Stte:Que foi manhee...";
-                sentences[2] = "A gente vive trancado nesse lugar... Precisamos sair daqui, tomar um pouco de ar... Vamos para praça!!";
+                sentences[1] = "(Valesca) Precisa de alguma coisa mãe? (Stte) Que foi manhee...";
+                sentences[2] = "A gente vive trancada nesse lugar... Precisamos sair daqui, tomar um pouco de ar... Vamos para praça!!";
             }
             else
             if (Persistence.Scene == 8)
@@ -97,15 +97,16 @@ public class DialogSystem : MonoBehaviour {
                 Character = Mae;
                 OtherPerson = RandomNpc1;
                 sentences = new string[9];
-                sentences[0] = "Olá dona Nilda, que bom que a senhora conseguiu trazer sua filhas para brincar!";
+                sentences[0] = "Olá dona Nilda, que bom que a senhora conseguiu trazer suas filhas para brincar!";
                 sentences[1] = "Foi dificil mas eu tirei elas de casa!";
-                sentences[2] = "Esse é um otimo lugar pra suas filha se desenvolverem, e brincarem.";
+                sentences[2] = "Esse é um otimo lugar pra suas filhas brincarem e se divertir de maneira saudavél";
                 sentences[3] = "Que bom que eu encontrei vocês!!";
-                sentences[4] = "Nosso trabalho é garantir que a criança possa brincar livre e em segurança, sair é bom pra saude sabia ??";
-                sentences[5] = "Sabia hehehe";
-                sentences[6] = "Queremos melhorar a qualidade de vida e a saúde dessas crianças.E contamos com a ajuda de muita gente para isso";
-                sentences[7] = "Como que podem ajudar?";
-                sentences[8] = "Precisamos principalmente de voluntários, doações de brinquedos e roupas, e podem contribuir monetariamente também";
+                sentences[4] = "Nosso trabalho é garantir que a criança possa brincar livre e em segurança, sair é bom pra saude sabia?";
+                sentences[5] = "É verdade";
+                sentences[6] = "Queremos melhorar a qualidade de vida e a saúde dessas crianças.";
+                sentences[7] = "Vocês precisam de ajuda? Teria alguma coisa que eu possa fazer?";
+                sentences[8] = "Então toda ajuda é bem vida!"; 
+                    //Precisamos de voluntários, doações de brinquedos e roupas, e podem contribuir monetariamente também";
             }
             if (Persistence.Scene == 9)
             {
